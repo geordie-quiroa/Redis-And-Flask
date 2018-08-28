485e67988816464804ae5876c9c8a0d45e1335a6
Geordie Quiroa <39976279+geordie-quiroa@users.noreply.github.com>
2018-08-28 09:06:09 -0600

    changed conn to localhost

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 import redis
 from flask import Flask, render_template, request
 app = Flask(__name__)
-conn = redis.StrictRedis(host='209.97.137.35', port='261')
+conn = redis.StrictRedis(host='localhost', port='261')
 
 @app.route('/', methods = ['GET', 'POST'])
 def homepage():
